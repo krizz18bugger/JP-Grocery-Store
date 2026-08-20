3daf3a398cb702943e37bfcfce57023516c76254
krizz18bugger <moorthi18krishna@gmail.com>
2026-08-20 10:03:04 +0530

    Error in BASE_URL was rectified

--- admin-app/src/api/api.js
+++ admin-app/src/api/api.js
@@ -1,1 +1,3 @@
-export const BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+import { URL } from 'url';
+const full_url = import.meta.env.VITE_API_URL || 'http://localhost:5000';
+export const BASE_URL = new URL('/api', full_url).href;
